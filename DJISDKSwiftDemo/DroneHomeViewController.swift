6490bbf1ec41832638142e9a2ed914cef1def38a
Noman Tassawar <nomantassawar1998@gmail.com>
2020-12-22 22:46:09 +0500

    Added navigation Drawer on Home Screen..

--- DJISDKSwiftDemo/DroneHomeViewController.swift
+++ DJISDKSwiftDemo/DroneHomeViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 import DJIUXSDK
 
 class DroneHomeViewController: DUXDefaultLayoutViewController {
-
+    
+    let transition = SlideInTransition()
+    
+    @IBOutlet weak var menuLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuLabel.layer.cornerRadius = 3.0
+        menuLabel.layer.masksToBounds = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapFunction))
+       menuLabel.addGestureRecognizer(tap)
 
         // Do any additional setup after loading the view.
     }
+    
     
 
     /*
@@ -27,5 +35,46 @@ class DroneHomeViewController: DUXDefaultLayoutViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @objc func tapFunction(sender:UITapGestureRecognizer) {
+        print("tapped  button menu")
+        
+               let menuVC = storyboard?.instantiateViewController(withIdentifier: "menuScreen") as! MenuViewController
 
+              menuVC.modalPresentationStyle = .overCurrentContext
+               menuVC.transitioningDelegate = self
+           present(menuVC, animated: true) {
+               print("Presented ViewController")
+           }
+    }
+    
+    
+    
+//    @IBAction func menuTapped(_ sender: UIButton) {
+//        print("tapped  button menu")
+//
+//            let menuVC = storyboard?.instantiateViewController(withIdentifier: "menuScreen") as! MenuViewController
+//
+//           menuVC.modalPresentationStyle = .overCurrentContext
+//            menuVC.transitioningDelegate = self
+//        present(menuVC, animated: true) {
+//            print("Presented ViewController")
+//        }
+//
+//
+//
+//
+//    }
+    
+    
+    
+}
+extension DroneHomeViewController: UIViewControllerTransitioningDelegate{
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.isPresenting = true
+        return transition
+    }
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.isPresenting = false
+        return transition
+    }
 }
